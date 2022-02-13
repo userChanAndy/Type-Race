258fe196e851abee5cf9dce64a4b66ae52a484d9
Andy Chan <andy.chan9930@gmail.com>
2022-02-13 18:25:42 -0500

    final Push

--- type-racer/src/components/HomePage.js
+++ type-racer/src/components/HomePage.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react";
 
 function HomePage() {
   return (
-    <div id = "HomePage">
-      <h1 className='intro'>Welcome to Keyboard Racer</h1>
-      <p className='introBody'>The key to typing faster is to practice. This app is designed for people who wish to type faster but dont know where to start. Create an account to save your scores and watch your words per minute race to the sky!</p>
+    <div id="HomePage">
+      <h1 className="intro">Welcome to Keyboard Racer</h1>
+      <p className="introBody">
+        The key to typing faster is to practice. This app is designed for people
+        who wish to type faster but dont know where to start. Keep playing this
+        game and watch your words per minute race to the sky!
+      </p>
       <br></br>
-      <h1 className='intro'>Navigating Type Racer</h1>
-      <p className='introBody'>Above you will see a navigation bar. We are currently in the Home page. Click race to play the game, Data to see your scores and the leaderboard and settings to update your profile.</p>        
+      <h1 className="intro">Navigating Type Racer</h1>
+      <p className="introBody">
+        Above you will see a navigation bar. We are currently in the Home page.
+        Click race to play the game, Data to see your scores.
+      </p>
     </div>
-)}
+  );
+}
 
 export default HomePage;
